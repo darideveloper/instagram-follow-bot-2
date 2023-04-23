8aa7547ab5a4478bf41c05462ddd4ac73258dc36
darideveloper <darideveloper@gmail.com>
2023-04-23 15:53:40 -0600

    feat (runs): don't catch errors

--- runs/unfollow.py
+++ runs/unfollow.py
@@ -6,17 +6,5 @@ sys.path.append(parent_folder)
 
 from bot import Bot
 
-try:
-    bot_instance = Bot()
-    bot_instance.auto_follow ()
-except Exception as err:   
-    exception_type, exception_object, exception_traceback = sys.exc_info()
-    filename = exception_traceback.tb_frame.f_code.co_filename
-    line_number = exception_traceback.tb_lineno
-
-    print (f"\n\tERROR: {err}")
-    print("Exception type: ", exception_type)
-    print("File name: ", filename)
-    print("Line number: ", line_number)
-    
-    input ("Press Enter to exit...")
+bot_instance = Bot()
+bot_instance.auto_follow ()
